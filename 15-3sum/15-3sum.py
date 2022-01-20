8842e38b3c73659deb352f4cc2610b7fa4e71927
SrushtiKhot <65593265+SrushtiKhot@users.noreply.github.com>
2022-01-19 20:26:18 -0500

    Time: 869 ms (56.87%), Space: 16.9 MB (36.02%) - LeetHub

---
+++ 15-3sum/15-3sum.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def threeSum(self, nums):
+        res=[]
+        nums.sort()
+        for i,n in enumerate(nums):
+            if(i>0 and nums[i-1]==n): #skip if duplicates occur [-3,-3,1,2] skip -3 encountered at 1
+                continue
+                
+            #Two sum on remaining elements
+            l,r=i+1,len(nums)-1
+            
+            while l<r:
+                if(n+nums[l]+nums[r]>0): 
+                    r-=1
+                elif(n+nums[l]+nums[r]<0):
+                    l+=1
+                    
+                else:
+                    res.append([n,nums[l],nums[r]])
+                    l+=1
+                    
+                    while(nums[l]==nums[l-1] and l<r):  #if same elements encountered,keep going
+                        l+=1   
+                    
+        return res
+            
+                    
+        
+        
+        
