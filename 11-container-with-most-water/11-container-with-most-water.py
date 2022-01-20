fff6d81b39b2014b38dc75b4bc22be71cdc5e99c
SrushtiKhot <65593265+SrushtiKhot@users.noreply.github.com>
2022-01-19 22:50:11 -0500

    Time: 1225 ms (15.01%), Space: 27.6 MB (58.07%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -5,17 +5,20 @@ class Solution:
         mc=0
         
         while(l<=r):
-            amount=(min(height[l],height[r]))*(r-l)
+            amount=(min(height[l],height[r]))*(r-l) 
             mc=max(amount,mc)
             
             #Incrementing pointers
-            if(height[l]<=height[r]):
+            if(height[l]<=height[r]): #increment which has lower value as we need max amount container
                 l+=1
                 
             else:
                 r-=1
                 
         return(mc)
+    
+    #O(n) Time complexity
+    #O(1) Space complexity
             
             
             
