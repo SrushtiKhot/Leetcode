6bb80fe3384fb07626992738f948dd968efe0ac8
SrushtiKhot <65593265+SrushtiKhot@users.noreply.github.com>
2022-01-20 23:59:30 -0500

    Time: 68 ms (7.88%), Space: 14.2 MB (85.47%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -4,16 +4,18 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         l,r=0,len(matrix)-1
-        t,b=l,r
+        t,b=l,r  #As it's a square matrix, top,bottom is same as left,right
 
         while(l<r):
-            for i in range(r-l):
+            for i in range(r-l): 
                 topleft=matrix[t][l+i]
                 matrix[t][l+i]=matrix[b-i][l]
                 matrix[b-i][l]=matrix[b][r-i]
                 matrix[b][r-i]=matrix[t+i][r]
                 matrix[t+i][r]=topleft
-                
+            
+            
+            #To execute above code for inner matrix
             r-=1
             l+=1
             t+=1
@@ -21,7 +23,8 @@ class Solution:
 
 
 
-        
+        #O(n) Time complexity
+        #O(1) Space complexity
         
         
         
