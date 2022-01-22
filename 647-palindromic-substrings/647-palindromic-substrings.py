2e62edf88f711dd4bbb8aa472a065ca65384b574
SrushtiKhot <65593265+SrushtiKhot@users.noreply.github.com>
2022-01-22 18:33:04 -0500

    Time: 214 ms (47.22%), Space: 14.1 MB (94.53%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -2,13 +2,13 @@ class Solution:
     def countSubstrings(self, s: str) -> int:
         res=0
         for i in range(len(s)):
-            l,r=i,i
+            l,r=i,i         #To count palindrome in odd length
             while(l>=0 and r<len(s) and s[l]==s[r]):
                     res+=1
                     l-=1
                     r+=1
             
-            l,r=i,i+1
+            l,r=i,i+1      #To count palindrome in even numbers
             while(l>=0 and r<len(s) and s[l]==s[r]):
                     res+=1
                     l-=1
@@ -16,3 +16,5 @@ class Solution:
                 
         return res
         
+        #O(n^2) Time complexity
+        #O(1) Space complexity
