fa5cf9c45778c00da8a8c63ce8664827f7f0ee5a
SrushtiKhot <65593265+SrushtiKhot@users.noreply.github.com>
2022-01-22 23:28:59 -0500

    Time: 39 ms (38.00%), Space: 14.4 MB (32.24%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -5,20 +5,53 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool: 
-        
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        #Recursive
+        '''
         if not p and not q:
             return True
         if not p or not q:
             return False
-        
         if(p.val!=q.val):
             return False
-        
-        
         return(self.isSameTree(p.left,q.left) and
-        self.isSameTree(p.right,q.right))
-    
+        self.isSameTree(p.right,q.right)) 
     #O(n) Time complexity
     #O(logn) in balanced tree O(n) in unbalanced tree
+    '''
+        #Iterative
+    
+        def check(p, q):
+            # if both are None
+            if not p and not q:
+                return True
+            # one of p and q is None
+            if not q or not p:
+                return False
+            if p.val != q.val:
+                return False
+            return True
+        
+        deq = deque([(p, q),])
+        while deq:
+            p, q = deq.popleft()
+            if not check(p, q):
+                return False
+            
+            if p:
+                deq.append((p.left, q.left))
+                deq.append((p.right, q.right))
+                    
+        return True
+                
+    
+                
+                
+                
+
+            
+            
+            
+            
+            
         
