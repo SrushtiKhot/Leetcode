29a8961dc5c4ce06d2c0bd6fc94ea23feaf7c584
Srushti Khot <65593265+SrushtiKhot@users.noreply.github.com>
2022-01-23 16:10:46 -0500

    Time: 482 ms (25.46%), Space: 15.5 MB (78.74%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -2,19 +2,20 @@ class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         row=len(heights)
         col=len(heights[0])
-        atl=set()
-        pac=set()
+        atl=set() #can reach altantic
+        pac=set() #can reach pacific\
+        
         
         def dfs(r,c,visit,prev):
             if((r,c) in visit or r<0 or c<0 or r==row or c==col or heights[r][c]<prev):
                 return
             visit.add((r,c))
             #Perform dfs on it's neighbours
-            dfs(r+1,c,visit,heights[r][c])
+            dfs(r+1,c,visit,heights[r][c])  #heights[r][c] becomes prev of neighbours
             dfs(r-1,c,visit,heights[r][c])
             dfs(r,c+1,visit,heights[r][c])
             dfs(r,c-1,visit,heights[r][c])    
-        for c in range(col):
+        for c in range(col):   
             dfs(0,c,pac,heights[0][c])
             dfs(row-1,c,atl,heights[row-1][c])
         for r in range(row):
@@ -27,6 +28,8 @@ class Solution:
                     res.append([r,c])
                     
         return res
+    
+    #O(nm) Time complexity
 
 
         
