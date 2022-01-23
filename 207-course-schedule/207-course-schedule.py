98c57d84dcec90a3cc8b61c5a7df856bbad2bbab
SrushtiKhot <65593265+SrushtiKhot@users.noreply.github.com>
2022-01-23 14:51:39 -0500

    Time: 95 ms (84.83%), Space: 17.2 MB (38.82%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -5,23 +5,24 @@ class Solution:
         premap={i:[] for i in range(numCourses)}
         for crs,pre in prerequisites:  #premap created
             premap[crs].append(pre)
-        visit=set()
+        visit=set()   #Set to keep track which course is already taken
         def dfs(crs):
-            
-            if crs in visit:
+            if crs in visit: #There exists a loop
                 return False
-            if premap[crs]==[]:
+            if premap[crs]==[]: #If no prereq required to complete crs
                 return True
-            
-            visit.add(crs)
-            for pre in premap[crs]:
-                if not dfs(pre):
+            visit.add(crs)  #Mark course as visited
+            for pre in premap[crs]: #Run dfs on all prereq of course
+                if not dfs(pre): #If a single prereq of crs returs false,return false
                     return False
-            visit.remove(crs)
-            premap[crs]=[]
+            visit.remove(crs)   
+            premap[crs]=[]  #If dfs is true delete the prereq of course
             return True
-        
+    
         for crs in range(numCourses):
             if not dfs(crs):
                 return False
         return True
+    
+    #O(n+m) n is numCourses and m is prerequistes Time complexity
+    
