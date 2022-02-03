df6bf65f342f90fcf5a2a09f96278ddf0c8e77fe
Srushti Khot <65593265+SrushtiKhot@users.noreply.github.com>
2022-02-03 11:07:09 -0500

    Time: 32 ms (76.46%), Space: 13.9 MB (90.53%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.py
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.py
@@ -4,12 +4,45 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+import collections
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        #Recursive solution 
-        if not root:
-            return []
-        return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
+        #Recursive solution
+        # res=[]
+        # if root:   #Check if root is not null
+        #         res.append(root.val) 
+        #         self.preorderTraversal(root.left)
+        #         self.preorderTraversal(root.right)
+        # return res
+        # #O(h) height of tree Tiem complexity
+        # #O(h) Space complexity
+        #Iterative solution
+        stack=[]
+        res=[]
+        stack.append(root)
+        while stack:
+            node=stack.pop()
+            if node:
+                res.append(node.val)
+                stack.append(node.right)
+                stack.append(node.left)
+                
+        return res
+
+        
+        
+        
+        
+        
+       
+        
+        
+        
+        
+        
+        
+        
+        
 
 
         
