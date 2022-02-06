e61b3eedee64856dfb2df5a820807a93392225c8
Srushti Khot <65593265+SrushtiKhot@users.noreply.github.com>
2022-02-06 12:53:36 -0500

    Time: 472 ms (60.35%), Space: 26 MB (49.31%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,9 +1,22 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        hashmap={}
-        for i,n in enumerate(nums):
-            if n in hashmap:
-                return True
-            hashmap[n]=i        
-        return False
+      #Brute force approach 
+      #Time complexity O(n^2) and space complexity is O(1) as we just return True/False
+#         if not nums:
+#             return False
+#         for i in range(len(nums)):
+#             for j in range(i+1,len(nums)):
+#                 if nums[i]==nums[j]:
+#                     return True 
+#         return False
+
+        return len(nums)!=len(set(nums))
+                    
+                    
+                
+                
+            
+            
+                
+    
         
