abc6ef58219686871ba4a5041dab79250fb43071
Srushti Khot <65593265+SrushtiKhot@users.noreply.github.com>
2022-02-06 20:55:20 -0500

    Time: 2155 ms (25.14%), Space: 14.1 MB (88.64%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,19 +1,22 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        cnt  = collections.Counter(s1)
+        cnt={}
+        for s in s1:
+            if s in cnt:
+                cnt[s]+=1
+            else:
+                cnt[s]=1
+              
+        #cnt  = collections.Counter(s1)
         l = 0 
-        r = 0
-        
-        while (r<len(s2)):
-            
+        r = 0   
+        while (r<len(s2)):   
             if(r-l+1 < len(s1)):
-                r+=1
-                
+                r+=1       
             elif(r-l+1 == len(s1)):
                 if(collections.Counter(s2[l:r+1]) == cnt):
                     return True
                 r+=1
-                l+=1
-                
+                l+=1            
         return False
         
