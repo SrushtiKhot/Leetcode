b369b3ede8382870f3d54a2b7b6709d201877b37
Srushti Khot <65593265+SrushtiKhot@users.noreply.github.com>
2022-02-13 22:03:24 -0500

    Time: 36 ms (59.95%), Space: 13.9 MB (80.51%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -6,52 +6,31 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        #Recursive
-        '''
-        if not p and not q:
-            return True
-        if not p or not q:
-            return False
-        if(p.val!=q.val):
-            return False
-        return(self.isSameTree(p.left,q.left) and
-        self.isSameTree(p.right,q.right)) 
-    #O(n) Time complexity
-    #O(logn) in balanced tree O(n) in unbalanced tree
-    '''
-        #Iterative
-    
-        def check(p, q):
-            # if both are None
-            if not p and not q:
-                return True
-            # one of p and q is None
-            if not q or not p:
-                return False
-            if p.val != q.val:
-                return False
-            return True
         
-        deq = deque([(p, q),])
-        while deq:
-            p, q = deq.popleft()
-            if not check(p, q):
+        #Recursion 
+#         if(p==None and q==None):
+#             return True
+#         if(p==None or q==None):
+#             return False
+#         if(p.val!=q.val):
+#             return False
+        
+#         return(self.isSameTree(p.right,q.right) and self.isSameTree(p.left,q.left))
+    
+    
+        #Using stack
+        stack=[(p,q)]
+        while stack:
+            p,q=stack.pop()
+            if not p and not q:
+                continue
+                
+            if(not p or not q) or (p.val!=q.val):
                 return False
             
-            if p:
-                deq.append((p.left, q.left))
-                deq.append((p.right, q.right))
-                    
+            stack.extend([(p.left,q.left),(p.right,q.right)])
+            
         return True
                 
-    
-                
-                
-                
-
-            
-            
-            
-            
-            
+        
         
