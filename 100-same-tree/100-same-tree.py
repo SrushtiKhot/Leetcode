0db619c51798a70f169da391fd03f516eeb1c19b
Srushti Khot <65593265+SrushtiKhot@users.noreply.github.com>
2022-02-13 22:19:28 -0500

    Time: 48 ms (34.12%), Space: 13.9 MB (94.56%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -6,19 +6,45 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        
-        #Recursion 
-#         if(p==None and q==None):
-#             return True
-#         if(p==None or q==None):
-#             return False
-#         if(p.val!=q.val):
-#             return False
-        
-#         return(self.isSameTree(p.right,q.right) and self.isSameTree(p.left,q.left))
+        #Recursive
+        '''
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if(p.val!=q.val):
+            return False
+        return(self.isSameTree(p.left,q.left) and
+        self.isSameTree(p.right,q.right)) 
+    #O(n) Time complexity
+    #O(logn) in balanced tree O(n) in unbalanced tree
+    '''
+        #Iterative
     
-    
-        #Using stack
+        def check(p, q):
+            # if both are None
+            if not p and not q:
+                return True
+            # one of p and q is None
+            if not q or not p:
+                return False
+            if p.val != q.val:
+                return False
+            return True
+        
+        deq = deque([(p, q),])
+        while deq:
+            p, q = deq.popleft()
+            if not check(p, q):
+                return False
+            
+            if p:
+                deq.append((p.left, q.left))
+                deq.append((p.right, q.right))
+                    
+        return True
+
+        #Using stack  T.C- O(h) Height of tree
         stack=[(p,q)]
         while stack:
             p,q=stack.pop()
@@ -33,4 +59,16 @@ class Solution:
         return True
                 
         
+        
+                
+    
+                
+                
+                
+
+            
+            
+            
+            
+            
         
