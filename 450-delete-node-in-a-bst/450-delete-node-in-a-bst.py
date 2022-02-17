ed82506d871c614470dc0f90cd42275a45913f77
Srushti Khot <65593265+SrushtiKhot@users.noreply.github.com>
2022-02-17 11:07:26 -0500

    Time: 93 ms (56.74%), Space: 18.4 MB (86.74%) - LeetHub

---
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.py
@@ -0,0 +1,52 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def succ(self,root): #Leftmost child of right subtree
+        root=root.right
+        while(root.left):
+            root=root.left      
+        return root.val
+    
+    def pred(self,root): #Rightmost child of left subtree
+        root=root.left
+        while(root.right):
+            root=root.right
+        return root.val
+    
+    def deleteNode(self,root,key):
+        if not root:
+            return None
+        
+        if key< root.val:
+            #left subtree
+            root.left=self.deleteNode(root.left,key)
+            
+        elif key>root.val:
+            #Right subtree
+            root.right=self.deleteNode(root.right,key)
+            
+        else: #key==root.val delete the current node
+            if not(root.right or root.left):
+                #leaf node
+                root=None
+                
+            else: #Not leaf node
+                if root.right:
+                    root.val=self.succ(root)                        #Substitute root's value with it's successor
+                    root.right=self.deleteNode(root.right,root.val) #delete the successor node recursively
+                else:
+                    root.val=self.pred(root)                        #Substitute root's value with it's successor
+                    root.left=self.deleteNode(root.left,root.val)   #delete the successor node recursively  
+        return root
+                    
+                    
+                    
+            
+            
+            
+        
+    
