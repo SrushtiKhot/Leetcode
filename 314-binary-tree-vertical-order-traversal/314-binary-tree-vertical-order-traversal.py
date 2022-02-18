d3320f17734927470f8d4aeb95cd77b72350c436
Srushti Khot <65593265+SrushtiKhot@users.noreply.github.com>
2022-02-18 00:25:49 -0500

    Time: 45 ms (54.69%), Space: 13.9 MB (94.85%) - LeetHub

--- 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.py
@@ -6,25 +6,59 @@
 #         self.right = right
 import collections
 class Solution:
+    #O(nlogn) Time complexity --> n for number of nodes and nlogn for sorting
+    #O(n) Sapce complexity
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        # if not root:
+        #     return
+        # count=defaultdict(list) #nodes with horizontal distances
+        # res=[]
+        # q=collections.deque()
+        # q.append((root,0)) #hd of root is 0
+        # while q:
+        #     node,hd=q.popleft()
+        #     count[hd].append(node.val)
+        #     if node.left:
+        #         q.append((node.left,hd-1))
+        #     if node.right:
+        #         q.append((node.right,hd+1))
+        # d=sorted(count.items()) #Sort dictionary on the basis of key
+        # d1=dict(d).values()
+        # for i in d1:
+        #     res.append(i)
+        # return(res)
+        
+        # return [count[i] for i in sorted(count.keys())]
+    
+#Optimal approach
+#Time complexity O(N) No sorting is required
+#O(n) to store hash
+    
         if not root:
-            return
-        count=defaultdict(list) #nodes with horizontal distances
-        res=[]
+            return []
+        minimum,maximum=0,0
         q=collections.deque()
-        q.append((root,0)) #hd of root is 0
+        q.append((root,0)) #0 is col number
+        count=defaultdict(list)
         while q:
             node,hd=q.popleft()
-            count[hd].append(node.val)
-            if node.left:
+            if node:
+                count[hd].append(node.val)
+                minimum=min(hd,minimum)  #To calculate the range of min and max hashindex count
+                maximum=max(hd,maximum)
                 q.append((node.left,hd-1))
-            if node.right:
-                q.append((node.right,hd+1))          
-        d=sorted(count.items()) #Sort dictionary on the basis of key
-        d1=dict(d).values()
-        for i in d1:
-            res.append(i)
-        return(res)
+                q.append((node.right,hd+1))
+
+        return([count[i] for i in range(minimum,maximum+1)])
+            
+           
+            
+            
+            
+            
+            
+        
+        
     
                 
         
