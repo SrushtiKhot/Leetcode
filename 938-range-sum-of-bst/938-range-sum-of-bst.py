f7af06ab4c1674c9a2c2311e7d3409c02158d051
Srushti Khot <65593265+SrushtiKhot@users.noreply.github.com>
2022-02-23 14:10:16 -0500

    Time: 313 ms (48.17%), Space: 23 MB (23.87%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.py
+++ 938-range-sum-of-bst/938-range-sum-of-bst.py
@@ -8,23 +8,45 @@ import collections
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
         #Level order traversal
-        if not root:
-            return     
-        res=[]
-        s=0  
-        q=collections.deque()
-        q.append(root)
-        while q:
-            node=q.popleft()
-            res.append(node.val)
-            if node.left:
-                q.append(node.left)
-            if node.right:
-                q.append(node.right)               
-        if res:
-            for i in res:
-                if i in range(low,high+1):
-                    s+=i
-        return s
+#         if not root:
+#             return     
+#         res=[]
+#         s=0  
+#         q=collections.deque()
+#         q.append(root)
+#         while q:
+#             node=q.popleft()
+#             res.append(node.val)
+#             if node.left:
+#                 q.append(node.left)
+#             if node.right:
+#                 q.append(node.right)               
+#         if res:
+#             for i in res:
+#                 if i in range(low,high+1):
+#                     s+=i
+#         return s
         
+         #Using L and R
+        if not root:
+            return
+        s=0
+        stack=[root]
+        while stack:
+            node=stack.pop()
+            if node:
+                if low<=node.val<=high:
+                    s+=node.val
+                if node.val>low:
+                    stack.append(node.left)
+                if node.val<high:
+                    stack.append(node.right)
+
+        return s
+            
+            
+            
+        
+        
+    
         
