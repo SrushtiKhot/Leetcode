29d6cddf4d74889c55efac8963fad4447545e704
Srushti Khot <65593265+SrushtiKhot@users.noreply.github.com>
2022-02-23 23:34:29 -0500

    Time: 81 ms (65.34%), Space: 18.5 MB (50.40%) - LeetHub

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.py
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -10,20 +10,28 @@ class Node:
 
 class Solution:
     def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
-        hash1=[]
-        hash2=[]
-        while p:
-            hash1.append(p)
-            p=p.parent
+        #Time complexity O(hp+hq) #Space complexity O(hp+hq)
+#         hash1=[]
+#         hash2=[]
+#         while p:
+#             hash1.append(p)
+#             p=p.parent    
+#         while q:
+#             hash2.append(q)
+#             q=q.parent      
+#         for i in hash1:
+#             if i in hash2:
+#                 return i
+
+         #Time complexity O(hp+hq) Space complexity O(1)
+        pp,qp=p,q
+        while pp!=qp:
+            pp=pp.parent if pp.parent else q
+            qp=qp.parent if qp.parent else p
             
-            
-        while q:
-            hash2.append(q)
-            q=q.parent
-            
-        for i in hash1:
-            if i in hash2:
-                return i
+        return pp
+          
+        
                 
         
         
