52385b93fc78ad762dcc0f7f5a4da4e1204a8ba4
Srushti Khot <65593265+SrushtiKhot@users.noreply.github.com>
2022-02-27 16:41:45 -0500

    Time: 55 ms (24.80%), Space: 13.8 MB (92.32%) - LeetHub

--- 408-valid-word-abbreviation/408-valid-word-abbreviation.py
+++ 408-valid-word-abbreviation/408-valid-word-abbreviation.py
@@ -1,9 +1,6 @@
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
         w,a=0,0
-        
-        
-        
         while(w<len(word) and a<len(abbr)):
         #If alphabet
             if abbr[a].isalpha():
@@ -12,15 +9,19 @@ class Solution:
                     a+=1
                 else:
                     return False
+                
             
-            elif abbr[a].isdigit():
+            else:
                 if abbr[a]=='0':
                     return False
                 temp=0
                 
-                while(a<len(abbr) and abbr[a].isdigit()):
+                while(a<len(abbr) and abbr[a].isdigit()): 
                     temp=temp*10+(int)(abbr[a])
                     a+=1
                 w+=temp
         return (a==len(abbr) and w==len(word))
+    
+    #Time complexity O(max(len(word),len(abbr)))
+    #Space complexity O(1)
         
