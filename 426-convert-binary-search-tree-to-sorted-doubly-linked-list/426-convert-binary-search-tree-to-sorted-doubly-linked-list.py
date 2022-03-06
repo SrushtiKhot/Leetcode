8d0d8bc212e990bb23043e4f8c53c15fb5bf5bd1
Srushti Khot <65593265+SrushtiKhot@users.noreply.github.com>
2022-03-05 23:59:05 -0500

    Time: 57 ms (38.01%), Space: 14.7 MB (93.63%) - LeetHub

--- 426-convert-binary-search-tree-to-sorted-doubly-linked-list/426-convert-binary-search-tree-to-sorted-doubly-linked-list.py
+++ 426-convert-binary-search-tree-to-sorted-doubly-linked-list/426-convert-binary-search-tree-to-sorted-doubly-linked-list.py
@@ -9,22 +9,52 @@ class Node:
 
 class Solution:
     def treeToDoublyList(self, root: 'Node') -> 'Node':
-        def helper(self,node):      
-            if node:
-                helper(self,node.left)              
-                self.prev.right=node
-                node.left=self.prev
-                self.prev=node             
-                helper(self,node.right)
+        # def helper(self,node):      
+        #     if node:
+        #         helper(self,node.left)              
+        #         self.prev.right=node
+        #         node.left=self.prev
+        #         self.prev=node             
+        #         helper(self,node.right)     
+        # if not root:
+        #     return None    
+        # self.head=Node(0)
+        # self.prev=self.head
+        # helper(self,root)
+        # self.prev.right = self.head.right
+        # self.head.right.left=self.prev
+        # return self.head.right
         
-        if not root:
-            return None
+        #Iterative Approach
         
-        self.head=Node(0)
-        self.prev=self.head
-        helper(self,root)
-        self.prev.right = self.head.right
-        self.head.right.left=self.prev
-        return self.head.right
+       
+        if not root: 
+            return root
+    
+        stack = []
+        curr = root
+        prev = head = None
+
+        while stack or curr:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            node = stack.pop()
+            node.left = prev
+
+            if prev:
+                prev.right = node
+            prev = node
+
+            if not head: 
+                head = node
+
+            curr = node.right
+
+        head.left = prev
+        prev.right = head
+
+        return head
+
             
         
