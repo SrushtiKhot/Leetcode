ebb647cc51314ea847e5a782eebee5e2e7af0182
Srushti Khot <65593265+SrushtiKhot@users.noreply.github.com>
2022-03-07 11:15:53 -0500

    Time: 1526 ms (14.42%), Space: 20.4 MB (49.77%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -1,9 +1,80 @@
 from collections import defaultdict
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        res=sorted(points,key=lambda x:(x[0]*x[0])+x[1]*x[1])
-        print(res)
-        return res[:k]
+        dist=[]
+        remaining=[]
+        closest=[]
+        
+        for p in points:
+            dist.append(self.euclidean(p))
+            
+        for p in range(len(points)):
+            remaining.append(p)
+            
+        low=0
+        high=max(dist)
+            
+        while k:
+            mid=(low+high)/2
+            
+            closer,farther=self.splitlist(remaining,dist,mid)
+            if len(closer)>k:
+                high=mid
+                remaining=closer
+                
+            else:
+                k-=len(closer)
+                closest.extend(closer)
+                remaining=farther
+                low=mid
+                
+                
+        return [points[i] for i in closest]
+                
+       
+    def euclidean(self,point):
+        return(pow(point[0],2)+pow(point[1],2))
+    
+    def splitlist(self,remaining,dist,mid):
+        closer=[]
+        farther=[]
+        for r in remaining:
+            if dist[r]<=mid:
+                closer.append(r)
+            else:
+                farther.append(r)
+                
+        return([closer,farther])
+        
+        
+        
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+#         res=sorted(points,key=lambda x:(x[0]*x[0])+x[1]*x[1]) #Sorts points list taking dist as key
+#         print(res)
+#         return res[:k]
+            
         
             
     
