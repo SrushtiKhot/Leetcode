b3983b65b64f29aaf9a06f1bd49b800dfa643e8c
Srushti Khot <65593265+SrushtiKhot@users.noreply.github.com>
2022-03-16 20:23:54 -0400

    Time: 88 ms (86.27%), Space: 15.5 MB (68.06%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.py
+++ 227-basic-calculator-ii/227-basic-calculator-ii.py
@@ -1,13 +1,24 @@
 class Solution:
     def calculate(self, s):
-        s += '+0'
-        stack, num, preOp = [], 0, "+"
+        if not s:
+            return "0"
+        stack, num, sign = [], 0, "+"
         for i in range(len(s)):
-            if s[i].isdigit(): num = num * 10 + int(s[i])
-            elif not s[i].isspace():
-                if   preOp == "-":  stack.append(-num)
-                elif preOp == "+":  stack.append(num)
-                elif preOp == "*":  stack.append(stack.pop() * num)
-                else:               stack.append(int(stack.pop() / num))
-                preOp, num = s[i], 0
+            if s[i].isdigit():
+                num = num*10+ord(s[i])-ord("0")
+            if (not s[i].isdigit() and not s[i].isspace()) or i == len(s)-1:
+                if sign == "-":
+                    stack.append(-num)
+                elif sign == "+":
+                    stack.append(num)
+                elif sign == "*":
+                    stack.append(stack.pop()*num)
+                else:
+                    tmp = stack.pop()
+                    if tmp//num < 0 and tmp%num != 0:
+                        stack.append(tmp//num+1)
+                    else:
+                        stack.append(tmp//num)
+                sign = s[i]
+                num = 0
         return sum(stack)
