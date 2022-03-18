3b14ca0fbee12c92de2b3461bb72f41278285cb1
Srushti Khot <65593265+SrushtiKhot@users.noreply.github.com>
2022-03-18 00:05:28 -0400

    Time: 144 ms (38.41%), Space: 15.4 MB (76.27%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.py
+++ 227-basic-calculator-ii/227-basic-calculator-ii.py
@@ -1,24 +1,70 @@
 class Solution:
     def calculate(self, s):
+        last=0
+        res=0
+        opr='+'
+        curr=0       
         if not s:
-            return "0"
-        stack, num, sign = [], 0, "+"
+            return 0       
         for i in range(len(s)):
-            if s[i].isdigit():
-                num = num*10+ord(s[i])-ord("0")
-            if (not s[i].isdigit() and not s[i].isspace()) or i == len(s)-1:
-                if sign == "-":
-                    stack.append(-num)
-                elif sign == "+":
-                    stack.append(num)
-                elif sign == "*":
-                    stack.append(stack.pop()*num)
-                else:
-                    tmp = stack.pop()
-                    if tmp//num < 0 and tmp%num != 0:
-                        stack.append(tmp//num+1)
+            ch=s[i]          
+            if ch.isdigit():
+                curr=(curr*10)+(ord(ch)-ord('0'))
+                print(curr)
+            
+            if not ch.isdigit() and not ch.isspace() or i==len(s)-1:  
+                if opr=='+' or opr=='-':
+                    res+=last
+                    if opr=='+':
+                        last=curr
+                        print(last)
+                    elif opr=='-':
+                        last=-curr
+                        print(last)
+                                
+                elif opr=='*':
+                    last=last*curr
+                elif opr=='/':
+                    if (last/curr)<0 and last%curr !=0:
+                        last=last//(curr)+1
                     else:
-                        stack.append(tmp//num)
-                sign = s[i]
-                num = 0
-        return sum(stack)
+                        last=last//(curr)     
+                opr=ch
+                curr=0
+            
+        res=res+last 
+        return res
+    
+    #O(n) Time complexity 
+    #O(1) Space complexity --> Stack
+    
+              
+        
+#         if not s:
+#             return "0"
+#         stack, num, sign = [], 0, "+"
+#         for i in range(len(s)):
+#             if s[i].isdigit():
+#                 num = num*10+ord(s[i])-ord("0")
+#             if (not s[i].isdigit() and not s[i].isspace()) or i == len(s)-1:
+#                 if sign == "-":
+#                     stack.append(-num)
+#                 elif sign == "+":
+#                     stack.append(num)
+#                 elif sign == "*":
+#                     stack.append(stack.pop()*num)
+#                 else:
+#                     tmp = stack.pop()
+#                     if tmp//num < 0 and tmp%num != 0:
+#                         stack.append(tmp//num+1)
+#                     else:
+#                         stack.append(tmp//num)
+#                 sign = s[i]
+#                 num = 0
+#         return sum(stack)
+    
+    #O(n) Time complexity 
+    #O(n) Space complexity --> Stack
+    
+    
+    
